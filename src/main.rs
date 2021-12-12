a450b5de4404b324e6e8a645a2938c418bf7568b
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2021-12-12 18:08:47 +0900

    apply cargo fmt and cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::str::FromStr;
 
-use mrbgpdv2::peer::Peer;
 use mrbgpdv2::config::Config;
+use mrbgpdv2::peer::Peer;
 
 #[tokio::main]
 async fn main() {
     let configs = vec![Config::from_str("64512 127.0.0.1 65413 127.0.0.2 active").unwrap()];
-    let mut peers: Vec<Peer> = configs.into_iter().map(|c| Peer::new(c)).collect();
+    let mut peers: Vec<Peer> = configs.into_iter().map(Peer::new).collect();
     for peer in &mut peers {
         peer.start();
     }
