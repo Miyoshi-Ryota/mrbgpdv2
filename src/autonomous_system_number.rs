bff1b148ea4af86729b6ae41885dc6d65e53d515
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2021-12-23 06:57:42 +0900

    消し忘れてたファイルを削除

--- src/autonomous_system_number.rs
+++
@@ -1,8 +0,0 @@
-#[derive(PartialEq, Eq, Debug, Clone, Copy, Hash, PartialOrd, Ord)]
-pub struct AutonomousSystemNumber(u16);
-
-impl From<u16> for AutonomousSystemNumber {
-    fn from(as_number: u16) -> Self {
-        Self(as_number)
-    }
-}
