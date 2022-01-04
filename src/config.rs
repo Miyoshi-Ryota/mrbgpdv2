3576d5716bd09c932722f9dd27daf13897c4764a
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2022-01-04 13:54:21 +0900

    configにnetwork command, 広報するルートを指定するコマンドを実装した

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,18 @@
 use crate::bgp_type::AutonomousSystemNumber;
 use crate::error::ConfigParseError;
 use anyhow::{Context, Result};
+use ipnetwork::Ipv4Network;
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 
-#[derive(PartialEq, Eq, Debug, Clone, Copy, Hash, PartialOrd, Ord)]
+#[derive(PartialEq, Eq, Debug, Clone, Hash, PartialOrd, Ord)]
 pub struct Config {
     pub local_as: AutonomousSystemNumber,
     pub local_ip: Ipv4Addr,
     remote_as: AutonomousSystemNumber,
     pub remote_ip: Ipv4Addr,
     pub mode: Mode,
+    pub networks: Vec<Ipv4Network>,
 }
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy, Hash, PartialOrd, Ord)]
@@ -55,12 +57,20 @@ impl FromStr for Config {
             "cannot parse 5th part of config, `{0}`, as as-number and config is {1}",
             config[4], s
         ))?;
+        let mut networks: Vec<Ipv4Network> = vec![];
+        for network in &config[5..] {
+            networks.push(network.parse().context(format!(
+                "cannot parse config[5..], `{0}` as Ipv4Network and config is {1}",
+                network, s
+            ))?)
+        }
         Ok(Self {
             local_as,
             local_ip,
             remote_as,
             remote_ip,
             mode,
+            networks,
         })
     }
 }
